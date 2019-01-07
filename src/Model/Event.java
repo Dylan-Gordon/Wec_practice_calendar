ff4e09df284e8b271dacde7bc23a10f9ec88a2d9
Unknown <ewan.may@ucalgary.ca>
2019-01-07 09:39:26 -0700

    change event

--- src/Model/Event.java
+++ src/Model/Event.java
@@ -6,10 +6,32 @@ public class Event {
 	private Time startTime;
 	private Time duration;
 	
-	public Event() {
-		// TODO Auto-generated constructor stub
+	public Event(Date startDate, Time startTime, Time duration) {
+		super();
+		this.startDate = startDate;
+		this.startTime = startTime;
+		this.duration = duration;
 	}
-
+	
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Time getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(Time startTime) {
+		this.startTime = startTime;
+	}
+	public Time getDuration() {
+		return duration;
+	}
+	public void setDuration(Time duration) {
+		this.duration = duration;
+	}
+	
 	
 	
 }
